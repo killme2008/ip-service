e02f6720060c105f69ae4e597005aaed9956fa66
dennis zhuang <killme2008@gmail.com>
2014-10-16 14:51:53 +0800

    Released version-3

--- ip-service/src/ip_service/handler.clj
+++ ip-service/src/ip_service/handler.clj
@@ -1,134 +1,49 @@
 (ns ip-service.handler
-  (:use org.clojars.smee.binary.core)
   (:require [compojure.core :refer :all]
             [compojure.handler :as handler]
             [hbs.core :as hbs]
-            [clojure.java.io :as io]
-            [clojure.string :as cs]
+            [ip-service.ip :as ip-service]
             [clojure.data.json :as json]
-            [compojure.route :as route])
-  (:import [java.nio ByteBuffer MappedByteBuffer ByteOrder]
-           [java.io FileInputStream ByteArrayInputStream InputStream]
-           [java.net InetAddress]
-           [java.nio.channels FileChannel FileChannel$MapMode]))
+            [compojure.route :as route]))
 
-(def ip-codec
-  "IP structures codec"
-  (compile-codec
-   (repeated
-    {:ip-num :int-be
-     :offset-bs (repeated :byte :length 3)
-     :size :byte})))
-
-(defn- buffer-stream
-  "Create a InputStream form a ByteBuffer instance."
-  [^ByteBuffer buf]
-  (proxy [InputStream] []
-    (read
-      ([]
-         (if (.hasRemaining buf)
-           (bit-and 0xFF (.get buf))
-           -1))
-      ([bs]
-         (read bs 0 (alength bs)))
-      ([bs off len]
-         (if (.hasRemaining buf)
-           (let [len (Math/min len (.remaining buf))]
-             (.get buf off len)
-             len)
-           -1)))))
-
-(defn- ip->long
-  "Create long value from ip raw address."
-  [ip]
-  (->> ip
-       (InetAddress/getByName)
-       (.getAddress)
-       (io/input-stream)
-       (decode :int-be)))
-
-(defn- load-data
-  "Load IP database as s MappedByteBuffer."
-  []
-  (let [^FileChannel fc
-        (->
-         (FileInputStream.
-          (io/file (io/resource "17monipdb.dat")))
-         (.getChannel))]
-    (.map fc FileChannel$MapMode/READ_ONLY 0 (.size fc))))
-
-(defn get-int
-  "Get int32 from byte buffer"
-  [buf pos endian]
-  (-> buf
-      (.position pos)
-      (.order endian)
-      (.getInt)))
-
-(defn- init
-  "Initialize ip-service"
-  []
-  (let [data (load-data)
-        length (.getInt data)]
-    {:data-len length :buf data}))
-
-;; An atom to keep IP database.
-(defonce ip-data (init))
-
-(defn find-geography [ip]
-  (when-not (= (count (cs/split ip #"\.")) 4)
-    (throw (ex-info "Only supports IPv4." {:ip ip})))
-  (let [ip-head (-> ip (cs/split #"\.") first (Integer/parseInt))
-        ipn (ip->long ip)
-        {:keys [buf data-len]} ip-data
-        ;;make a copy of buffer for thread-safe
-        buf (.duplicate buf)
-        start (-> buf
-                  (get-int (+ 4 (* 4 ip-head)) ByteOrder/LITTLE_ENDIAN)
-                  (* 8)
-                  (+ 4)
-                  (+ 1024))
-        max-comp-len (- data-len 1024 4)]
-    (some
-     (fn [{:keys [ip-num offset-bs size]}]
-       (when (>= ip-num ipn)
-         (let [offset (-> offset-bs
-                          (vec)
-                          (conj 0) ;;Padding zero at end.
-                          (byte-array)
-                          (ByteBuffer/wrap)
-                          (.order ByteOrder/LITTLE_ENDIAN)
-                          (.getInt))]
-           (if (<= offset 0)
-             (throw (ex-info "Invalid result offset." {:offset offset}))
-             (decode (string "utf-8" :length size)
-                     (buffer-stream
-                      (.position buf
-                                 (-> offset (+ data-len) (- 1024)))))))))
-     (decode ip-codec
-             (buffer-stream
-              (-> buf
-                  (.duplicate)
-                  (.order ByteOrder/BIG_ENDIAN)
-                  (.position start)))))))
+(defn json-response [body & {:keys [status headers] :or {status 200}}]
+  (merge
+   {:status status
+    :headers
+    (merge
+     {"Content-Type" "application/json;charset=utf-8"}
+     headers)
+    :body (json/write-str body)}))
 
 (defn query-api
   "A handler to query IP geography."
   [req]
   (let [ip (-> req :params :ip)]
-    {:status 200
-     :headers { "Content-Type" "application/json;charset=utf-8"}
-     :body (json/write-str
-            {:ip ip
-             :result (find-geography ip)})}))
+    (json-response
+     {:ip ip
+      :result (ip-service/find-geography ip)})))
 
 (defn query-page [req]
-  (let [ip (or (-> req :params :ip)
-               (:remote-addr req))]
+  (let [ip (or (-> req :params :ip))]
     (hbs/render-file "index"
                      {:ip ip
                       :result (when ip
-                                (find-geography ip))})))
+                                (ip-service/find-geography ip))})))
+
+;;middlewares
+(defn json-req? [{content-type :content-type}]
+  (and content-type
+       (re-find #"(?i)application/json" content-type)))
+
+(defn- wrap-error-handler [handler]
+  (fn [req]
+    (try
+      (handler req)
+      (catch Throwable e
+        (if (json-req? req)
+          (json-response {:error (.getMessage e)} :status 400)
+          {:status 400
+           :body (hbs/render-file "index" {:error (.getMessage e)})})))))
 
 (defroutes app-routes
   (GET "/" [] query-page)
@@ -138,4 +53,7 @@
   (route/not-found "Not Found"))
 
 (def app
-  (handler/site app-routes))
+  (handler/site
+   (->
+    app-routes
+    wrap-error-handler)))
--- ip-service/test/ip_service/test/handler.clj
+++ ip-service/test/ip_service/test/handler.clj
@@ -1,14 +1,42 @@
 (ns ip-service.test.handler
   (:require [clojure.test :refer :all]
             [ip-service.handler :refer :all]
+            [clojure.data.json :as json]
             [ring.mock.request :as mock]))
+
+(defn json-request
+  "Mock json request."
+  [method uri & params]
+  (-> (apply mock/request method uri params)
+      (update-in  [:headers]
+             #(merge % {"Content-Type" "application/json"}))
+      (assoc :content-type "application/json")))
 
 (deftest test-app
   (testing "main route"
     (let [response (app (mock/request :get "/"))]
       (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
-  
+      (is (re-find #"<h2>IP Service</h2>" (:body response))))
+    (let [response (app (mock/request :get "/?ip=127.0.0.1"))]
+      (is (= (:status response) 200))
+      (is (re-find #"127.0.0.1  &nbsp;&nbsp;&nbsp;   本机地址	本机地址" (:body response))))
+    (let [response (app (mock/request :get "/?ip=180.117.51.245"))]
+      (is (= (:status response) 200))
+      (is (re-find #"180.117.51.245  &nbsp;&nbsp;&nbsp;   中国	江苏	苏州	" (:body response))))
+    (let [response (app (mock/request :get "/?ip=localhost"))]
+      (is (= (:status response) 400))
+      (is (re-find #"Illegal address,only supports IPv4." (:body response)))))
+
+  (testing "REST API"
+    (let [response (app (json-request :get "/1/ip?ip=180.117.51.245"))]
+      (is (= (:status response) 200))
+      (is (= "application/json;charset=utf-8" (get-in response [:headers "Content-Type"])))
+      (is (= {:ip "180.117.51.245" :result "中国\t江苏\t苏州\t"} (json/read-str (:body response) :key-fn keyword))))
+    (let [response (app (json-request :get "/1/ip?ip=0:0:0:0:0:0"))]
+      (is (= (:status response) 400))
+      (is (= "application/json;charset=utf-8" (get-in response [:headers "Content-Type"])))
+      (is (= {:error "Illegal address,only supports IPv4."} (json/read-str (:body response) :key-fn keyword)))))
+
   (testing "not-found route"
     (let [response (app (mock/request :get "/invalid"))]
       (is (= (:status response) 404)))))
